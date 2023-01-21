7f2b2d2077c6accf6c824825997d8f6eb9cfff1a
cassio.v.guerra <cassioguerra@live.com>
2023-01-21 11:19:04 -0300

    adiciona novasaolicaçõe

---
+++ src/test/java/com/PIMS/PIMS/PimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PIMS.PIMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
